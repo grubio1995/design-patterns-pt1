1530fa8d649ad2ecee430445761a3da8fc2f0905
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-06-26 21:12:30 -0300

    Ações após gerar um pedido

---
+++ src/br/com/loja/alura/pedido/acao/EnviarEmailPedido.java
@@ -0,0 +1,11 @@
+package br.com.loja.alura.pedido.acao;
+
+import br.com.loja.alura.pedido.Pedido;
+
+public class EnviarEmailPedido {
+	
+	public void executar(Pedido pedido) {
+		System.out.println("Salvando pedidos no banco de dados!");
+	}
+
+}
---
+++ src/br/com/loja/alura/pedido/acao/SalvarPedidoNoBancoDeDados.java
@@ -0,0 +1,11 @@
+package br.com.loja.alura.pedido.acao;
+
+import br.com.loja.alura.pedido.Pedido;
+
+public class SalvarPedidoNoBancoDeDados {
+	
+	public void executar(Pedido pedido) {
+		System.out.println("Enviando email com os dados do pedido ");
+	}
+
+}
